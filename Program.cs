71536a5de99641f2499a94e6e98a6260f7273e02
Amina <aminchik2809@gmail.com>
2024-04-27 21:20:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ProgramPrintingMyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Амина";
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
